18116754440eca17a211c3dcde95948e80293361
RicardoZiTseng <1941651789@qq.com>
2018-07-21 21:53:28 +0800

    start to write the heap sort algorithm

---
+++ Heap/heap.py
@@ -0,0 +1,44 @@
+"""
+author: Ricardo
+pages of CLRS: chinese page: page 84 in chapter 6
+
+"""
+
+def parent(i):
+    """
+    return the index of the i's parent
+    """
+    return i // 2
+
+def left(i):
+    """
+    return the i's left brother's index
+    """
+    return 2*i
+
+def right(i):
+    """
+    return the i's right brother's index
+    """
+    return 2*i + 1
+
+def heapsize(A):
+    return len(A) - 1
+
+def max_heapify(A, i):
+    """
+    keep the nature of heap structure
+    """
+    l = left(i)
+    r = right(i)
+    largest = 0
+    if l <= heapsize(A) and A[l] >= A[i]:
+        largest = l
+    else:
+        largest = i
+    if r <= heapsize(A) and A[r] >= A[i]:
+        largest = r
+    if largest != i:
+        A[i], A[largest] = A[largest], A[i]
+        max_heapify(A, largest)
+    return A
